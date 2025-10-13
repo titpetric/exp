8e37f3c856b4d17d665214f5f46de466ee4c636b
Tit Petric <black@titpetric.com>
2025-10-13 14:03:08 +0200

    Log complexity in lines

--- cmd/go-fsck/stats/sqlite/schema.sql
+++ cmd/go-fsck/stats/sqlite/schema.sql
@@ -62,7 +62,8 @@ CREATE TABLE `funcs` (
     `receiver` TEXT,
     `signature` TEXT,
     `complexity_cognitive` INTEGER,
-    `complexity_cyclomatic` INTEGER
+    `complexity_cyclomatic` INTEGER,
+    `complexity_lines` INTEGER,
 );
 CREATE INDEX `idx_funcs_pkg` ON `funcs`(`package_id`);
 CREATE INDEX `idx_funcs_file` ON `funcs`(`file`);
